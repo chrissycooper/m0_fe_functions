6f7dd6562f656124ad931842f032a1d11e874458
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-11-10 12:29:54 -0600

    Clean up

--- cleanUp.js
+++ cleanUp.js
@@ -9,38 +9,45 @@
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
+//I wasn't sure about this one, I added semi colons. Otherwise it looks pretty good to me
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
+//This one I cleaned up the indenting to make it easier to tell what each line belongs to. e.g. the bottom } now lines up with its function, and the codeblock is now indented to that function so you can tell it's inside the function. 
+
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
+//similarly, here I bumped down the second semicolon so its easy to see where the fcn ends. I added a space after the parentheses and completed the word function from "func"
+
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
-    var avg = sum / 2;
+    var average = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+  console.log(`the average is: ${average}.`);
+}
+
+//somemore bracket arranging, this time at the top as well as the bottom,and added a semicolon. Then I noticed the function name is clipped in the code block and the console.log, so I edited it to match. Then I ran it, and found an error that was pointing to the last parenthesis in the console.log. But, that looked okay. That's when I noticed the period was outside of the backtick. I moved it, and ran the code, and then it looked good!
